f78a17c903e6f9e2b8ed92c47eedadc926930196
Yahya Sahaja <yahya.sahaja2@gmail.com>
2017-09-15 19:31:23 +0700

    delete uglify plugin

--- webpack.config.js
+++ webpack.config.js
@@ -49,20 +49,6 @@ module.exports = {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         }),
-        new webpack.optimize.UglifyJsPlugin({
-            mangle: true,
-            compress: {
-                warnings: false,
-                pure_getters: true,
-                unsafe: true,
-                unsafe_comps: true,
-                screw_ie8: true
-            },
-            output: {
-                comments: false,
-            },
-            exclude: [/\.min\.js$/gi]
-        }),
         new CompressionPlugin({
             asset: "[path].gz[query]",
             algorithm: "gzip",
