7579ab9be2f68da36571ce21c549729c81bdba77
Yahya Sahaja <yahya.sahaja2@gmail.com>
2017-09-19 22:40:27 +0700

    fix BUG!!!!

--- src/app/component/panel/Drop/Password.js
+++ src/app/component/panel/Drop/Password.js
@@ -4,7 +4,7 @@ import PasswordInput from './PasswordInput'
 export default class Password extends Component {
     render() {
         const data = this.props.data;
-        let hari, tanggal, bulan, tahun, deadline, date;
+        let hari, tanggal, bulan, tahun, deadline, date, jam, menit;
         if (data) if (data.deadline) {
             deadline = new Date(data.deadline);
             hari = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'][deadline.getDay()];
