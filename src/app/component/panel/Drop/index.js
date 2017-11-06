49443bec178e9003c1c9d8dc135808281ed94d54
Yahya Sahaja <yahya.sahaja2@gmail.com>
2017-11-06 18:34:12 +0700

    producion

--- src/app/component/panel/Drop/index.js
+++ src/app/component/panel/Drop/index.js
@@ -20,7 +20,7 @@ export default class Drop extends Component {
     }
 
     loadSponsor = () => {
-        axios.get('/json/sponsor.json').then(({data}) => {
+        axios.get('https://bcc-drophere.herokuapp.com/json/sponsor.json').then(({data}) => {
             let res = [], remain = 0;
             for (let i = 0; i < data.length; i++) if (data[i].special) {
                 res.push(data[i]);
